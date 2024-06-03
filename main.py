5c9a738995e773482cdbf7e719f7edb4308a2a29
Nikes74 <umka813790@ya.ru>
2024-06-04 02:16:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# Задача «Длина слова».
+a =  'Hello, world!'
+print(len(a))
+
+# Задача «Суммы и разности»
+first = int(8)
+second = int(15)
+summa = first + second
+diff = first - second
+print (summa, diff)
+
+# Задача «Среднее арифметическое».
+first = 4
+second = 9
+third = 27
+mean = (first+second+third)/3
+print(mean)
+
+# Задача «Простые строчки»
+first_string = 'Вторник'
+second_string = "Понедельник"
+print(second_string+', '+first_string)
+
+# Задача «Сложная формула»
+a = 5
+b = 12
+c = 67
+f = (a * b) + (a * c)
+print ((f**3)/2)
